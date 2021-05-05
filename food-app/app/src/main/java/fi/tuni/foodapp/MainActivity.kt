ca41642502bda5c0abaa9194ec90970ecdb6ca7e
Tero Jussinniemi <tero.jussinniemi@gmail.com>
2021-05-05 19:16:16 +0300

    Initial project files

---
+++ food-app/app/src/main/java/fi/tuni/foodapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package fi.tuni.foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
